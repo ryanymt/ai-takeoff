b3cc64b6b7901d0c882e3471b5d5fc2a4c15a31f
Boo Kian Yeo <bookian@google.com>
2024-11-07 12:33:48 +0800

    added \ in comment

--- terrable/modules/tenant/scripts/persistent-resource.sh
+++ terrable/modules/tenant/scripts/persistent-resource.sh
@@ -9,11 +9,12 @@ create_resource() {
         --display-name=ai-takeoff \
         --project=${project_id} \
         --region=${region} \
-#	--resource-pool-spec="replica-count=3,min-replica-count=3,max-replica-count=4,machine-type=a2-ultragpu-1g,accelerator-type=NVIDIA_A100_80GB,accelerator-count=1,disk-type=pd-ssd,disk-size=100"
-# L4#	--resource-pool-spec="replica-count=3,min-replica-count=3,max-replica-count=4,machine-type=g2-standard-8,accelerator-type=NVIDIA_L4,accelerator-count=1,disk-type=pd-ssd,disk-size=100"
-# test 
-        --resource-pool-spec="replica-count=1,min-replica-count=1,max-replica-count=3,machine-type=n1-standard-4,disk-type=pd-ssd,disk-size=100"
-#    --resource-pool-spec="replica-count=1,min-replica-count=1,max-replica-count=3,machine-type=n1-standard-4,accelerator-type=ACCELERATOR_TYPE,accelerator-count=1,disk-type=pd-ssd,disk-size=100"
+	--resource-pool-spec="replica-count=3,min-replica-count=3,max-replica-count=4,machine-type=a2-ultragpu-1g,accelerator-type=NVIDIA_A100_80GB,accelerator-count=1,disk-type=pd-ssd,disk-size=100" \
+# L4#	--resource-pool-spec="replica-count=3,min-replica-count=3,max-replica-count=4,machine-type=g2-standard-8,accelerator-type=NVIDIA_L4,accelerator-count=1,disk-type=pd-ssd,disk-size=100" \
+# test \
+#     --resource-pool-spec="replica-count=1,min-replica-count=1,max-replica-count=3,machine-type=n1-standard-4,disk-type=pd-ssd,disk-size=100" \
+#    --resource-pool-spec="replica-count=1,min-replica-count=1,max-replica-count=3,machine-type=n1-standard-4,accelerator-type=ACCELERATOR_TYPE,accelerator-count=1,disk-type=pd-ssd,disk-size=100" \
+
 
     while [ "${result}" != "RUNNING" ]; do
         sleep 10
@@ -35,7 +36,7 @@ project_id=$2
 region=$3
 resource_id=$4
 
-case "$command" in
+case "${command}" in
     create)
         create_resource
         ;;
